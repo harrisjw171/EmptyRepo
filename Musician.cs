d82d18c4caf4d68c842e417c2e0231e15e9df800
harrisjw171 <harrisjw171@gmail.com>
2022-08-24 13:05:19 -0500

    Add files via upload

---
+++ Musician.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OOPExercises.Classes
+{
+    class Musician
+    {
+        public string Name { get; set; }
+        public Guitar Guitar { get; set; }
+
+        public Musician(string name, Guitar guitar)
+        {
+            Name = name;
+            Guitar = guitar;
+        }
+        public void Play()
+        {
+            Console.WriteLine($"{Name} plays the {Guitar.Brand}, {Guitar.Price}");
+        }
+    }
+}
